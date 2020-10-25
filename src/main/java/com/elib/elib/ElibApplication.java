988ff0b178144b3b6c86c7cc6229db6af2f09c41
Amadeus9029 <965720890@qq.com>
2020-10-25 14:10:52 +0800

    init

---
+++ src/main/java/com/elib/elib/ElibApplication.java
@@ -0,0 +1,13 @@
+package com.elib.elib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElibApplication.class, args);
+	}
+
+}
